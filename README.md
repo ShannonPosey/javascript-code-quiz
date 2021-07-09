1306477849187ec376bc727cda1d42e647fce8da
shannon_p <snp1284kasey@aol.com>
2021-07-09 05:58:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Code Quiz
+
+An application is a coding assessment.
+
+## Description
+
+This project is a coding assessment to practice answering questions that an HR Manager might ask during an interview process.  This coding assessment is a timed quiz that concise of five multiple-choice questions.  This quiz keeps track of your score, and at the end of the evaluation, users will see what their score is and save their score.
+
+### Built With
+HTML
+CSS
+JavaScript
+
+![code quiz demo](./assets/img/)
+
+### Website
+
+## Author
+
+👤 **Shannon Posey**
+
+* GitHub: [@ShannonPosey](https://github.com/ShannonPosey)
+* LinkedIn: [@https:\/\/www.linkedin.com\/in\/shannonposey](https://linkedin.com/in/https:\/\/www.linkedin.com\/in\/shannonposey)
+
+## 🤝 Contributing
+
+Made by Shannon Posey
+
+## 📝 License
+
+Copyright © 2021 [Shannon Posey](https://github.com/ShannonPosey).<br/>
+
+***
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/****  Creating the Code Quiz ****/
+// create a quiz that has five questions
+//  * each question has a list of answer to select from
+//  * if user selects the correct answer display the word "Correct!"
+//  * if user selects the wrong answer (this would be an else statement)  display the word "Wrong!"
+//  * next question will appear after the questions have been answered
+// includes a timer in the upper right hand corner
+//  * using a countdown function with certain amount of time to take the quiz
+//  * time is subtract for the quiz if question is answer incorrectly (decrement count/er--)
+// include a "View Highscore" in the upper left hand corner
+//  * use increment count/er++ to add score together
+// when all the questions or timer reaches 0 game is over
+// when game is over the user can save their name and score
+// use the assignments 2-5 as resource to build the quiz
+
+/**** Styling the Code Quiz ****/
+// use assignment 1 to style the quiz
