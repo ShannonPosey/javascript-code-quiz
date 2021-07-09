c39beace3f9f1655ef9e449dee6088724f25d97f
shannon_p <snp1284kasey@aol.com>
2021-07-09 18:47:54 -0400

    added localStorage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,41 @@
 
 /**** Styling the Code Quiz ****/
 // use assignment 1 to style the quiz
+
+//Question for the code quiz
+var questions = [
+{
+    //question 1
+    question: "Commonly used data types do NOT include:",
+    answer: ["1. strings",  "2.booleans", "3. alerts", "4. numbers"],
+    correctAnswer: "",
+},
+{
+    // question 2
+    question: "The condition if/else statement is enclosed within _____:",
+    answer: ["1. quotes", "2. Curly brackets", "3.parentheses", "4. square brackets"],
+    correctAnswer:"",
+},
+{
+    // question 3
+    question: "Arrays in JavaScript can be used to store ___:",
+    answer: ["1. number and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    correctAnswer:""
+},
+{
+    // question 4
+    question: "String values must be enclosed within ___ when being assigned to variables",
+    answer: ["1. commas", "2. curly brackets", "3. quotes", "4. [parentheses"],
+    correctAnswer:""
+},
+{
+    // question 5
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: ["1. JavaScript", "2.terminal/bash", "3. for loops", "4. console.log"],
+    correctAnswer:"",
+}
+];
+
+// localStorage to keep track of the scores
+var saveScore = function () {};
+var loadScore = function () {};
